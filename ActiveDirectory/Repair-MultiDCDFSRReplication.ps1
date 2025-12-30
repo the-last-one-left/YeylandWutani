3c6d36715eefd1dcb9d619be76cb015b0bc69684
the-last-one-left <zachary.child@pacificoffice.com>
2025-12-29 18:11:57 -0800

    Update Repair-MultiDCDFSRReplication.ps1

--- ActiveDirectory/Repair-MultiDCDFSRReplication.ps1
+++ ActiveDirectory/Repair-MultiDCDFSRReplication.ps1
@@ -520,8 +520,23 @@ function Wait-ForSYSVOLInitialization {
 
 # Main script execution
 try {
-    Write-Host "`n=== Multi-DC DFSR Replication Repair ===" -ForegroundColor DarkYellow
-    Write-Host "Yeyland Wutani - Building Better Systems" -ForegroundColor Gray
+    # Show YW Banner
+    $logo = @(
+        "  __   _______   ___      _    _  _ ___   __      ___   _ _____ _   _  _ ___ "
+        "  \ \ / / __\ \ / / |    /_\  | \| |   \  \ \    / / | | |_   _/_\ | \| |_ _|"
+        "   \ V /| _| \ V /| |__ / _ \ | .`` | |) |  \ \/\/ /| |_| | | |/ _ \| .`` || | "
+        "    |_| |___| |_| |____/_/ \_\|_|\_|___/    \_/\_/  \___/  |_/_/ \_\_|\_|___|"
+    )
+    $tagline = "B U I L D I N G   B E T T E R   S Y S T E M S"
+    $border  = "=" * 81
+    Write-Host ""
+    Write-Host $border -ForegroundColor Gray
+    foreach ($line in $logo) { Write-Host $line -ForegroundColor DarkYellow }
+    Write-Host ""
+    Write-Host $tagline.PadLeft(62) -ForegroundColor Gray
+    Write-Host $border -ForegroundColor Gray
+    Write-Host ""
+    Write-Host "        Multi-DC DFSR Replication Repair" -ForegroundColor Gray
     Write-Host "Started: $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')`n" -ForegroundColor Gray
     
     Write-LogMessage "Restore Type: $RestoreType"
