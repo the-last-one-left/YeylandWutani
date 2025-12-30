648f9c16dbc3fd603da7a655f1930faef30c200b
the-last-one-left <zachary.child@pacificoffice.com>
2025-12-29 18:07:51 -0800

    Banner Add

--- ActiveDirectory/Get-ADReplicationStatus.ps1
+++ ActiveDirectory/Get-ADReplicationStatus.ps1
@@ -54,8 +54,27 @@ try {
     exit 1
 }
 
-Write-Host "`n=== AD Replication Status Monitor ===" -ForegroundColor DarkYellow
-Write-Host "Yeyland Wutani - Building Better Systems" -ForegroundColor Gray
+#region Banner
+function Show-YWBanner {
+    $logo = @(
+        "  __   _______   ___      _    _  _ ___   __      ___   _ _____ _   _  _ ___ "
+        "  \ \ / / __\ \ / / |    /_\  | \| |   \  \ \    / / | | |_   _/_\ | \| |_ _|"
+        "   \ V /| _| \ V /| |__ / _ \ | .`` | |) |  \ \/\/ /| |_| | | |/ _ \| .`` || | "
+        "    |_| |___| |_| |____/_/ \_\|_|\_|___/    \_/\_/  \___/  |_/_/ \_\_|\_|___|"
+    )
+    $tagline = "B U I L D I N G   B E T T E R   S Y S T E M S"
+    $border  = "=" * 81
+    Write-Host ""
+    Write-Host $border -ForegroundColor Gray
+    foreach ($line in $logo) { Write-Host $line -ForegroundColor DarkYellow }
+    Write-Host ""
+    Write-Host $tagline.PadLeft(62) -ForegroundColor Gray
+    Write-Host $border -ForegroundColor Gray
+    Write-Host ""
+}
+#endregion Banner
+
+Show-YWBanner
 Write-Host "Started: $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')`n" -ForegroundColor Gray
 
 # Get domain controllers
